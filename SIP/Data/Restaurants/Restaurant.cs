fbdf708f32e4b7d6b845dae5901e4d4e4bbf78e4
astronms <astronms@gmail.com>
2021-01-17 16:40:11 +0100

    Eddied RestaurantList

--- SIP/Data/Restaurants/Restaurant.cs
+++ SIP/Data/Restaurants/Restaurant.cs
@@ -17,6 +17,6 @@ namespace SIP.Data.Restaurants
 
         public string Address { get; set; }
         public string Description { get; set; }
-        public int Rating { get; set; }
+        public double Rating { get; set; }
     }
 }
