f41f60e603393abd99167bee56c12a6e5f850ddd
astronms <astronms@gmail.com>
2021-06-12 16:59:38 +0200

    added validation for lat/long and fixed map in edit page

--- SIP/Migrations/20210612131331_12-06-2021.Designer.cs
+++ SIP/Migrations/20210612131331_12-06-2021.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SIP;
 
 namespace SIP.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210612131331_12-06-2021")]
+    partial class _12062021
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
