38a72d2eff2f452d27342bb7b31810a57cd5258e
astronms <astronms@gmail.com>
2021-06-13 17:07:04 +0200

    Revert "Adding comments and rating"
    This reverts commit 798f716114370a70a516846cbac4e013cd3ad6df.

---
+++ SIP/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SIP.Data.Restaurants;
+
+namespace SIP
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public DbSet<Restaurant> Restaurants { get; set; }
+        public DbSet<Rating> Ratings { get; set; }
+        public DbSet<RestaurantHours> RestaurantHours { get; set; }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ SIP/Data/Restaurants/Rating.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SIP.Data.Restaurants
+{
+    public class Rating
+    {
+        public int Id { get; set; }
+        public Restaurant Restaurant { get; set; }
+        public int Score { get; set; }
+    }
+}
